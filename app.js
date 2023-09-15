062fa24435f852452c9108c52a4745bb1a7d349b
AsimEqubal <asimequbal.web@gmail.com>
2023-09-15 10:13:36 +0530

    SetTimeout practice

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 
-// function myClick(){
-//    document.getElementById('h').innerHTML = "My Camle"
-// }
-// const box = document.getElementById('h');
+function myClick(){
+   document.getElementById('h').innerHTML = "My Camle"
+}
+const box = document.getElementById('h');
 
-// box.addEventListener('click', () => {
-//   box.remove();
-// });
+box.addEventListener('click', () => {
+  box.remove();
+});
+/*-------------------------------------------*/
 box = document.getElementById('demo');
 box.addEventListener('click',() =>{
    document.getElementById('h').innerHTML = "Hi there i got clicked"
 })
+setTimeout(function(){
+   document.getElementById('h').style.display = 'none'
+}, 4000);
+
 
 
 
