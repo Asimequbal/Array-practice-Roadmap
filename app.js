e956897e42033b998d96812287f4479ede83b06d
AsimEqubal <asimequbal.web@gmail.com>
2023-09-17 00:34:35 +0530

    srting method

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 
-function myClick(){
-   document.getElementById('h').innerHTML = "My Camle"
-}
-const box = document.getElementById('h');
+// function myClick(){
+//    document.getElementById('h').innerHTML = "My Camle"
+// }
+// const box = document.getElementById('h');
 
-box.addEventListener('click', () => {
-  box.remove();
-});
+// box.addEventListener('click', () => {
+//   box.remove();
+// });
 /*-------------------------------------------*/
-box = document.getElementById('demo');
-box.addEventListener('click',() =>{
-   document.getElementById('h').innerHTML = "Hi there i got clicked"
-})
-setTimeout(function(){
-   document.getElementById('h').style.display = 'none'
-}, 4000);
+// box = document.getElementById('demo');
+// box.addEventListener('click',() =>{
+//    document.getElementById('h').innerHTML = "Hi there i got clicked"
+// })
+// setTimeout(function(){
+//    document.getElementById('h').style.display = 'none'
+// }, 4000);
+/*-------------------------------------------*/
+//  let text1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//  document.getElementsByTagName("p")[0].innerHTML = text1.length;
+let myName ='Arif'
+console.log(myName.length)
+// console.log(myName.toUpperCase())
+// console.log(myName.toLowerCase())
+// console.log(myName.slice(2 , 23))
+// console.log(myName.replace( 'if', 'mx'))
+let friend = "     Md Shamim     "
+console.log(friend.trim())
+
+
+
+
 
 
 
